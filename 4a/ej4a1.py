ebb95291563639f840d84db7414097169dcfe271
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-05-17 19:01:23 +0200

    fet

--- 4a/ej4a1.py
+++ 4a/ej4a1.py
@@ -1,20 +1,4 @@
 """
-Enunciado:
-Dadas dos listas de elementos, implementa una función llamada
-find_intersection(list_1, list_2) que retorne la intersección de ambas listas.
-
-Parámetros:
-    list_1 (List): Lista de elementos
-    list_2 (List): Lista de elementos
-
-Ejemplo:
-    Entrada:
-    list_1 = [1, 2, 3, 4, 5]
-    list_2 = [4, 5, 6, 7, 8]
-
-    Salida:
-    [4, 5]
-
 Enunciat:
 Donades dues llistes d'elements, implementa una funció anomenada
 find_intersection(list_1, list_2) que retorni la intersecció de les dues llistes.
@@ -38,11 +22,22 @@ list_2 = [4, 5, 6, 7, 8]
 
 
 def find_intersection(list_1, list_2):
-    # Write here your code
-    pass
+    set_1 = set(list_1)
+    set_2 = set(list_2)
+    interseccio = set_1 & set_2
+    return interseccio
+
+
+def find_intersection_without_function(list_1, list_2):
+     resultat = []
+
+     for x in list_1:
+          if x in list_2 and x not in resultat:
+               resultat.append(x)
+     return resultat
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 
-# print(find_intersection([1, 2, 3, 4], [3, 4, 5, 6]))
-# print(find_intersection(['apple', 'banana', 'orange'], ['banana', 'kiwi', 'apple']))
+#print(find_intersection([1, 2, 3, 4], [3, 4, 5, 6]))
+#print(find_intersection(['apple', 'banana', 'orange'], ['banana', 'kiwi', 'apple']))
